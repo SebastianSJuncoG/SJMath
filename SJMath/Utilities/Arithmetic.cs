58af934e4dc59b64e8c5e6456768a7a5a00e21ec
Sebastian J <jsebastiansteven@gmail.com>
2025-12-04 20:40:23 -0500

    En utilities, se agregaron los metodos de operaciones basicas, los cuales permiten sumar, restar, multiplicar y dividir todos los numeros que se encuentren en una lista

--- SJMath/Utilities/Arithmetic.cs
+++ SJMath/Utilities/Arithmetic.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -50,6 +51,70 @@ namespace SJMath.Utilities
         public static double SimpleDivision(double x1, double x2)
         {
             return x1 / x2;
+        }
+
+        /// <summary>
+        /// Permite sumar todos los números que se encuentren en la lista
+        /// </summary>
+        /// <param name="Values">Lista de números</param>
+        /// <returns>El resultado entre la suma de todos los números de la lista</returns>
+        public static double MultipleSum(List<double> Values)
+        {
+            double response = 0.0;
+            foreach (double item in Values)
+            {
+                response = response + item;
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Permite restar todos los números que se encuentren en la lista
+        /// </summary>
+        /// <param name="Values">Lista de números</param>
+        /// <returns>El resultado entre la resta de todos los números de la lista</returns>
+        public static double MultipleSubtraction(List<double> Values)
+        {
+            double response = 0.0;
+            foreach (double item in Values)
+            {
+                response = response - item;
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Permite multiplicar todos los números que se encuentren en la lista
+        /// </summary>
+        /// <param name="Values">Lista de números</param>
+        /// <returns>El resultado entre la multiplicación de todos los números de la lista</returns>
+        public static double MultipleMultiplication(List<double> Values)
+        {
+            double response = 0.0;
+            foreach (double item in Values)
+            {
+                response = response * item;
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Permite dividir todos los números que se encuentren en la lista
+        /// </summary>
+        /// <param name="Values">Lista de números</param>
+        /// <returns>El resultado entre la división de todos los números de la lista</returns>
+        public static double MultipleDivision(List<double> Values)
+        {
+            double response = 0.0;
+            foreach (double item in Values)
+            {
+                response = response * item;
+            }
+
+            return response;
         }
     }
 }

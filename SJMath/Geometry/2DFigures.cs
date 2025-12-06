e7488fa19fc8b2f49719f3d0b9bca5ef4f70941e
Sebastian J <jsebastiansteven@gmail.com>
2025-12-05 21:48:13 -0500

    Se agregaron regiones para separar las diferentes figuras 2D Se agregaron los metodos para calcular el area, el perimetro y la diagonal (o hipotenusa) de un rectangulo

--- SJMath/Geometry/2DFigures.cs
+++ SJMath/Geometry/2DFigures.cs
@@ -10,6 +10,7 @@ namespace SJMath.Geometry
 {
     public static class _2DFigures
     {
+        #region Cuadrado
         /// <summary>
         /// Permite calcular el area de un cuadrado
         /// </summary>
@@ -37,5 +38,59 @@ namespace SJMath.Geometry
 
             return result;
         }
+
+        /// <summary>
+        /// Permite calcular la diagonal interna de un cuadrado
+        /// </summary>
+        /// <param name="part">Lado del cuadrado</param>
+        /// <returns>La diagonal interna de un cuadrado</returns>
+        public static double DiagonalSquare(double part)
+        {
+            double result = part * Arithmetic.SquareRoot(part);
+
+            return result;
+        }
+        #endregion
+
+        #region Rectangulo
+        /// <summary>
+        /// Permite calcular el area de un rectangulo
+        /// </summary>
+        /// <param name="high">Alto del rectangulo</param>
+        /// <param name="width">Ancho del rectangulo</param>
+        /// <returns>El area del rectangulo</returns>
+        public static double RectangleArea(double high, double width)
+        {
+            double result = (Arithmetic.SimpleMultiplication(2, high) + Arithmetic.SimpleMultiplication(2, width));
+
+            return result;
+        }
+
+        /// <summary>
+        /// Permite calcular el perimetro de un rectangulo
+        /// </summary>
+        /// <param name="high">Alto del rectangulo</param>
+        /// <param name="width">Ancho del rectangulo</param>
+        /// <returns>El perimetro de un rectangulo</returns>
+        public static double RectanglePerimeter(double high, double width)
+        {
+            double result = Arithmetic.SimpleMultiplication(2, (high + width));
+
+            return result;
+        }
+
+        /// <summary>
+        /// permite calcular la diagonal de un rectangulo
+        /// </summary>
+        /// <param name="high">Alto del rectangulo</param>
+        /// <param name="width">Ancho del rectangulo</param>
+        /// <returns>La diagonal de un rectangulo</returns>
+        public static double DiagonalRectangle(double high, double width)
+        {
+            double result = Arithmetic.SimpleExponent(high, 2) + Arithmetic.SimpleExponent(width, 2);
+
+            return Arithmetic.SquareRoot(result);
+        }
+        #endregion
     }
 }
